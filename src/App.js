ee9db56ee826905b51bae26a0495a90aee960cae
jacobvdmei <5565595+jacobvdmei@users.noreply.github.com>
2024-12-15 11:42:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TextAnalyzer from './components/TextAnalyzer.js';
+import Doorloper from './components/Doorloper.js';
+import Opgave25 from './components/Opgave25.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Opgave25 />
+  );
+}
+
+export default App;
